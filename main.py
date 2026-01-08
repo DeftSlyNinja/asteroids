1b5898abb613589eeda587e87bb97ab1586fd727
DeftSlyNinja <christopher_masson@hotmail.com>
2026-01-08 11:14:53 -0800

    updated constants, added player class and circleshape class

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     myClock = pygame.time.Clock()
     dt = 0 # Delta Time
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = myClock.tick(60) / 1000
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
