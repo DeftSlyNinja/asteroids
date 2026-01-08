141007b225a137fd1be296e7bcad95e10c813344
DeftSlyNinja <christopher_masson@hotmail.com>
2026-01-08 11:42:05 -0800

    Updated game loop to use groups instead of individuals

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     myClock = pygame.time.Clock()
     dt = 0 # Delta Time
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
     while True:
         log_state()
@@ -15,8 +18,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for thing in updatable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = myClock.tick(60) / 1000
     
