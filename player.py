ca10bb9ec61918426f15f92cef3485eaa8e47460
DeftSlyNinja <christopher_masson@hotmail.com>
2026-01-09 11:31:45 -0800

    rate limited shots

--- player.py
+++ player.py
@@ -1,11 +1,12 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 import pygame
 
 class Player(CircleShape):
-    def __init__(self, x, y):
+    def __init__(self, x, y, shot_cooldown=0):
         super().__init__(x, y, PLAYER_RADIUS)
+        self.shot_cooldown = shot_cooldown
         self.rotation = 0
     
     def triangle(self):
@@ -24,6 +25,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shot_cooldown -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -43,5 +45,8 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
     
     def shoot(self):
+        if self.shot_cooldown > 0:
+            return
         newShot = Shot(self.position[0], self.position[1], SHOT_RADIUS)
         newShot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
